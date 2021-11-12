f327956932dff5d91747df2c6019b10f773c1662
fh-herrera <84977821+fh-herrera@users.noreply.github.com>
2021-11-12 07:14:45 -0500

    Crud from HTTP

--- backend/index.js
+++ backend/index.js
@@ -39,3 +39,29 @@ app.post('/users', function(req, res){
         res.json(status);
     })
 })
+
+// Actualizar totalmente un user en la DB
+app.put('/users/:id',function(req,res){
+    const uid = req.params.id;
+    const usuario = req.body;
+    dbU.updateUserTotally(uid, usuario, function(status){
+        res.json(status);
+    })
+});
+
+// Actualizar parcialmente un user en la DB
+app.patch('/users/:id',function(req,res){
+    const uid = req.params.id;
+    const usuario = req.body;
+    dbU.updateUserPartially(uid, usuario, function(status){
+        res.json(status);
+    })
+});
+
+// Borrar un user en la DB
+app.delete('/users/:id',function(req,res){
+    const uid = req.params.id;
+    dbU.deleteUser(uid, function(status){
+        res.json(status);
+    })
+});
