6f962d4beba14a8d096e024c45455afd790bd959
David angarita <josdeivi90@hotmail.com>
2021-11-28 20:10:29 -0500

    arreglo de llaves

--- backend/src/db/firebase.js
+++ backend/src/db/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./ciclorrutamintic-e7248879c6f3.json');
+const serviceAccount = require('./ciclorrutamintic-4c7fa91c0b51.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -9,5 +9,3 @@ admin.initializeApp({
 const db = admin.firestore();
 
 module.exports = db;
-
-
